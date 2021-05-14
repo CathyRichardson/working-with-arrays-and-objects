d40e3541cffb538ed703bddefb0c71f107299e04
Cathy Richardson <cathyanne08@gmail.com>
2021-05-13 21:00:55 -0600

    completed destructing.js

--- destructuring.js
+++ destructuring.js
@@ -22,7 +22,7 @@ var carDetails = {
 */
 
 //Code Here
-
+const { color, make, model, year } = carDetails;
 
 
 ////////// PROBLEM 2 //////////
@@ -33,9 +33,10 @@ var carDetails = {
   The property names are firstName, lastName, and title.
 */
 
-function greeting( obj ) {
+function greeting(obj) {
   //Code Here
-  
+  const { firstName, lastName, title } = obj;
+
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
   // Do not edit the code above.
@@ -55,20 +56,27 @@ function greeting( obj ) {
 
 //Code Here
 
+const totalPopulation = (obj) => {
+  const { utah, california, texas, arizona } = obj;
+  return utah + california + texas + arizona;
+}
 
 
 ////////// PROBLEM 4 //////////
 
 /*
-  Write a function called ingredients that will take in an object. 
-  This object will have 3 properties named carb, fat, and protein. 
-  The property values will be strings. 
-  Use object destructuring to save the property values to new variables. 
-  Push these new variables to an array and return the array. 
+  Write a function called ingredients that will take in an object.
+  This object will have 3 properties named carb, fat, and protein.
+  The property values will be strings.
+  Use object destructuring to save the property values to new variables.
+  Push these new variables to an array and return the array.
 */
 
 //Code Here
-
+const ingredients = function (obj) {
+  const { carb, fat, protein } = obj;
+  return [carb, fat, protein];
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -86,7 +94,15 @@ function greeting( obj ) {
 */
 
 //Code Here
+function largeNumbers({first, second, third}) {
+  return Math.min(first, second, third);
+} 
 
+// This is the same function, but destructured inside the function 
+// function largeNumbers(obj) {
+//   const {first, second, third} = obj;
+//   return Math.min(first, second, third);
+// } 
 
 
 ////////// PROBLEM 6 //////////
@@ -99,4 +115,23 @@ function greeting( obj ) {
 
 //Code Here
 
+const numberGroups = ({a, b, c}) => {
+  console.log(a, b, c)
+  console.log(typeof a)
+  console.log(a.length)
+   if (a.length > b.length && a.length > c.length) {
+    console.log('returning a')
+     return a
+   } else if (b.length > c.length && b.length > a.length) {
+    console.log('returning b')
+    return b
+   } else if (c.length > a.length && c.length > b.length) {
+     console.log('returning c')
+     return c
+   }
+   console.log("returning nothing")
+}
 
+// a: (3) [1, 2, 3]
+// b: [1]
+// c: (5) [1, 2, 3, 4, 5]
